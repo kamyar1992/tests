7b5bd6ca9ad95106aa182fbb0fc6f99883762af3
Kamyar Mazaheri Fard <83576326+kamyar1992@users.noreply.github.com>
2022-07-10 17:00:02 +0430

    first commited file

---
+++ 4. week 4/1. set_demo.py
@@ -0,0 +1,41 @@
+# unordered data type
+
+# you CANNOT define an empty set using literals
+
+emptySet = set()
+
+sampleSet = {1, 2, 'A', 'kamyar', 100}
+print(sampleSet)
+for x in range(10):  # if it writes on memory its order does not change
+    print(sampleSet)
+
+
+print("+"*40)
+odd = set(range(1, 50, 2))  # 25 members
+power = {1, 4, 9, 16, 25, 36, 49}  # 7 members
+
+print(odd.intersection(power))
+print(len(odd.intersection(power)))
+
+# *important: a set does not take repetitive members
+print(len(odd.union(power)))  # sum of two sets is 32 but it gives 28
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
