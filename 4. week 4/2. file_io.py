621bcf7090db05989d8f402de2e7952494d98029
Kamyar Mazaheri Fard <83576326+kamyar1992@users.noreply.github.com>
2022-07-11 13:33:37 +0430

    number 6 video

--- 4. week 4/2. file_io.py
+++ 4. week 4/2. file_io.py
@@ -16,3 +16,37 @@
 with open('sample.txt') as fid:  # it is a pythonic way
     for line in fid:
         print(line.strip())
+
+print("=" * 40)
+# .readline() : read first line then wait for another call to read the next line
+with open('sample.txt') as fid:
+    line = fid.readline()
+    while line:
+        print(line.strip())
+        line = fid.readline()
+
+print("=" * 40)
+# .readlines() : it return all lines as a list elements
+with open('sample.txt') as fid:
+    lines = fid.readlines()
+print(lines)
+
+
+print('=' * 40)
+with open('sample.txt') as fid:
+    rf = fid.read()
+    rfChar = fid.read(5)
+
+print(rf, '\n')
+print('=' * 40)
+with open('sample.txt') as fid:
+    rfChar = fid.read(5)
+print(rfChar)
+
+
+
+
+
+
+
+
