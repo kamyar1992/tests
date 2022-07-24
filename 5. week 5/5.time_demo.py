e396cca7dcca01710ab5339be2ffc906c55a26a6
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-07-24 12:19:21 +0430

    video 9

--- 5. week 5/5.time_demo.py
+++ 5. week 5/5.time_demo.py
@@ -2,12 +2,18 @@ import time
 
 print(time.time())
 print(time.gmtime(1658575345))
-print('='* 40)
+print('=' * 40)
+
 print(time.gmtime())
 print(time.gmtime(0))
-print('='* 40)
+print('=' * 40)
+
 print(time.localtime(0))
 print(time.localtime(1658575345))
 print(time.localtime(1000000000))
+
+print('=' * 40)
+print(time.gmtime(2147483648))  # when 32 bits system will stop working
+# print(time.gmtime(2305843009213693952))  # when 64 bits system will stop working
 
 
