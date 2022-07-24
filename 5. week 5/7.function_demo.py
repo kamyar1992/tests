04cad81ada49e95d4713a0b04637d86c435b6883
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-07-24 18:20:16 +0430

    video 15

--- 5. week 5/7.function_demo.py
+++ 5. week 5/7.function_demo.py
@@ -21,5 +21,19 @@ print(type(temp2))
 # 1- str()
 # 2- repr()
 
+def adder(a, b, c=1 , d=2):  # parameters
+    return a + b + c + d
 
 
+# non-default arguments ==> positional arguments
+# default arguments ==> keyword arguments
+addedNumbers = adder(1, 2, 3, 5)  # arguments
+print(addedNumbers)
+print(adder(1, 2))
+print(adder(1, 2, 4))
+print(adder(1, 2, d=4))
+print(adder(3, 3, c=1, d=1))
+print(adder(a=1, d=1, b=1, c=1))
+
+print('=' * 40)
+print(1, 2, 3, sep='@@', end='%%%')
