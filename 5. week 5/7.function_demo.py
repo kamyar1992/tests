f1a0b738a5c7a559d030b7bd32436003e531a919
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-07-24 17:26:28 +0430

    video 14

---
+++ 5. week 5/7.function_demo.py
@@ -0,0 +1,25 @@
+# def <functionName>(parameters):
+    # 1-
+    # 2-
+    # 3-
+
+def temp():
+    print('inside temp function')
+
+def temp2():
+    print('inside temp2 function')
+    return 'this is temp2 function'
+print(temp())
+print('=' * 40)
+temp()
+print('=' * 40)
+print(temp2())
+print(temp2)  # repr(): return identity address
+print(type(temp2))
+
+# print():
+# 1- str()
+# 2- repr()
+
+
+
