379a599aa3bd03ed73e42bd91453ce591a8a7e96
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-07-24 13:17:47 +0430

    video 10

--- 5. week 5/5.time_demo.py
+++ 5. week 5/5.time_demo.py
@@ -15,5 +15,24 @@ print(time.localtime(1000000000))
 print('=' * 40)
 print(time.gmtime(2147483648))  # when 32 bits system will stop working
 # print(time.gmtime(2305843009213693952))  # when 64 bits system will stop working
+print('=' * 40)
+
+timeList = ['monotonic', 'perf_counter', 'process_time', 'thread_time', 'time']
+for clock in timeList:
+    print('clock info for {}: {}'.format(clock, time.get_clock_info(clock)))
+    print('---------------------')
+
+print('=' * 40)
+
+tehranTime = time.localtime()
+print(tehranTime)
+print('{}, {}, {} \t {}, {}, {}'.format(tehranTime.tm_mday,
+                                        tehranTime.tm_mon,
+                                        tehranTime.tm_year,
+                                        tehranTime.tm_hour,
+                                        tehranTime.tm_min,
+                                        tehranTime.tm_sec))
+
+
 
 
