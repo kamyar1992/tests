cd402ce399092226048dd61b1b8da135958620a4
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-07-27 18:48:17 +0430

    video 6 generators and recursive methods

---
+++ 6. week 6/fibonacci_alternative.py
@@ -0,0 +1,11 @@
+def fibonacci(n):
+    previous, current = 0, 1
+    for _ in range(n):
+        # print(current)
+        previous, current = current, current + previous
+    return current
+
+
+if __name__ == '__main__':
+    print(len(str(fibonacci(100000))))
+
