faeda1154f265d1c1b31272d1ab673fed53335c4
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-08-06 12:28:49 +0430

    review

--- 5. week 5/sudoku_project.py
+++ 5. week 5/sudoku_project.py
@@ -1,18 +1,20 @@
 import turtle
 
-
+turtle.tracer()
 def set_starter():
-    turtle_object = turtle.Turtle()
-    screen_object = turtle.Screen()
-    turtle_object.hideturtle()
-    screen_object.setup(550, 550)
+
+    screen_object = turtle.Screen()  # make a screen
+    screen_object.setup(550, 550)    # resize the screen
+
+    turtle_object = turtle.Turtle()  # make turtle object
+    # turtle_object.hideturtle()       # hide turtle
     turtle_object.penup()
     turtle_object.goto(-225, 225)
     turtle_object.pendown()
-    turtle_object.speed(5)
+    # turtle_object.speed(5)
     turtle_object.pensize(5)
 
-    # turtle_object.speed('fastest')
+    turtle_object.speed('fastest')
     print(turtle_object.position())
     return turtle_object, screen_object
 
@@ -28,7 +30,7 @@ def draw_horizontal_lines(turtle_object):
         turtle_object.forward(450)
         turtle_object.penup()
         print(turtle_object.position())
-        print(float(turtle_object.position()[0]), turtle_object.position()[1])
+        # print(float(turtle_object.position()[0]), turtle_object.position()[1])
         turtle_object.goto(-turtle_object.position()[0], turtle_object.position()[1]-50)
         turtle_object.pendown()
     turtle_object = set_starter()[0]
@@ -42,16 +44,28 @@ def draw_horizontal_lines(turtle_object):
             turtle_object.pensize(1)
         turtle_object.forward(450)
         turtle_object.penup()
-        print(turtle_object.position()[0]+50, 225)
+        # print(turtle_object.position()[0]+50, 225)
         turtle_object.goto(turtle_object.position()[0]+50, 225)
         turtle_object.pendown()
 
 
-screen = set_starter()[1]
-draw_horizontal_lines(set_starter()[0])
-number = turtle.Turtle()
-number.hideturtle()
-number.write('1')
+def number_adder(turtle_object):
+    # turtle_object.hideturtle()
+
+    turtle_object.pendown()
+    print(turtle_object.isvisible())
+    turtle_object.forward(100)
+    turtle_object.write('1')
+
+
+draw, screen = set_starter()
+
+draw_horizontal_lines(draw)
+# number = turtle.Turtle()
+# number.hideturtle()
+# number.write('1')
+# number_adder(draw)
+print(draw.position())
 screen.exitonclick()
 
 
