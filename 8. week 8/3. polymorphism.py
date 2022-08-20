3bb33d4c7767860b253fcab14715d78855dc435b
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-08-20 14:37:31 +0430

    week 8 video 4

---
+++ 8. week 8/3. polymorphism.py
@@ -0,0 +1,20 @@
+# Polymorphism
+# https://realpython.com/oop-in-python-vs-java/
+
+a = 2
+b = 3.14
+c = 'test'
+
+# methods of the objects define their behaviours
+# none of the a, b and c objects has the print methods, but we can print them because every object in python inherit
+# from a class that is named Object.
+# Object class has a method that enable them to be printed __str__(). thus, inheritance make Polymorphism.
+print(a)
+print(b)
+print(c)
+
+# two ways for implementing Polymorphism:
+# composition, Aggregation
+
+# delegation
+
