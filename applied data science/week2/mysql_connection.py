023919a0d817c4d639392e045bf22b49d86a392f
Kamyar Mazaheri Fard <kamyar.k1992@gmail.com>
2022-09-16 14:22:31 +0430

    week 2, video 3

--- applied data science/week2/mysql_connection.py
+++ applied data science/week2/mysql_connection.py
@@ -8,6 +8,7 @@
 # remember to grant user access
 import mysql.connector
 from mysql.connector import connection
+from mysql.connector import errorcode
 # cn = mysql.connector.connect(user='admin',
 #                              password='P@ssw0rd',
 #                              host='192.168.43.128',
@@ -20,13 +21,26 @@ from mysql.connector import connection
 #                                 database='dayche')
 
 config = {"user": 'kamyar',
-          'password': 'Kmz19921371@',
+          'password': 'mz19921371@',
           'host': '192.168.43.128',
-          'database': 'dayche',
+          'database': 'db2',
           'raise_on_warnings': True}
-cn = connection.MySQLConnection(**config)  # *important remember ** before parameter
-cn.close()
+# cn = connection.MySQLConnection(**config)  # *important remember ** before parameter
+# cn.close()
 
+try:
+    cn = mysql.connector.connect(**config)
+except mysql.connector.Error as err:
+    if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:   # *important : pay attention to err.errno
+        print('access is denied')
+    elif err.errno == errorcode.ER_BAD_DB_ERROR:
+        print('database does not exist')
+    elif err.errno == errorcode.ER_USERNAME:
+        print('username or password in incorrect')
+    else:
+        print(err)
+else:
+    cn.close()
 
 
 
